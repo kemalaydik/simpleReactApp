5f7cbda642c0550b906aa3d2e481a34888b619a8
Kemal Aydık <kemalaydik@gmail.com>
2022-07-02 15:24:24 +0300

    .

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -2,72 +2,12 @@ import React, { useEffect, useState, useContext } from 'react';
 import Axios from 'axios';
 import Page from './Page';
 import DispatchContext from '../DispatchContext';
-import { useImmerReducer } from 'use-immer';
-import { CSSTransition } from 'react-transition-group';
-import { XCircleIcon } from '@heroicons/react/solid';
 
 function HomeGuest() {
 	const appDispatch = useContext(DispatchContext);
-	// const [username, setUsername] = useState();
-	// const [email, setEmail] = useState();
-	// const [password, setPassword] = useState();
-	const initialState = {
-		username: {
-			value: '',
-			hasErrors: false,
-			message: '',
-			inUnique: false,
-			checkCount: 0
-		},
-		email: {
-			value: '',
-			hasErrors: false,
-			message: '',
-			inUnique: false,
-			checkCount: 0
-		},
-		password: {
-			value: '',
-			hasErrors: false,
-			message: ''
-		},
-		submitCount: 0
-	};
-
-	function reducer(draft, action) {
-		switch (action.type) {
-			case 'usernameImmediately':
-				draft.username.hasErrors = false;
-				draft.username.value = action.value;
-				if (draft.username.length > 30) {
-					draft.username.hasError = true;
-					draft.username.message = 'Username cannot exceed 30 characters';
-				}
-				return;
-			case 'usernameAfterDelay':
-				return;
-			case 'usernameUniqueResults':
-				return;
-			case 'emailImmediately':
-				draft.email.hasErrors = false;
-				draft.email.value = action.value;
-				return;
-			case 'emailAfterDelay':
-				return;
-			case 'emailUniqueResults':
-				return;
-			case 'passwordImmediately':
-				draft.password.hasErrors = false;
-				draft.password.value = action.value;
-				return;
-			case 'passwordAfterDelay':
-				return;
-			case 'submitForm':
-				return;
-		}
-	}
-
-	const [state, dispatch] = useImmerReducer(reducer, initialState);
+	const [username, setUsername] = useState();
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
 
 	async function handleSubmit(e) {
 		e.preventDefault();
@@ -89,13 +29,13 @@ function HomeGuest() {
 				<div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
 					<div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
 						<form onSubmit={handleSubmit} className='space-y-6' action='#' method='POST'>
-							<div>
+							<div className='relative'>
 								<label htmlFor='username-register' className='block text-sm font-medium text-gray-700'>
 									Username
 								</label>
 								<div className='mt-1'>
 									<input
-										onChange={e => dispatch({ type: 'usernameImmediately', value: e.target.value })}
+										onChange={e => setUsername(e.target.value)}
 										id='username-register'
 										name='username'
 										type='text'
@@ -104,22 +44,6 @@ function HomeGuest() {
 										className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
 									/>
 								</div>
-
-								<div className='rounded-md bg-red-50 p-4'>
-									<div className='flex'>
-										<div className='flex-shrink-0'>
-											<XCircleIcon />
-										</div>
-										<div className='ml-3'>
-											<h3 className='text-sm font-medium text-red-800'>There were 2 errors with your submission</h3>
-											<div className='mt-2 text-sm text-red-700'>
-												<ul role='list' className='list-disc pl-5 space-y-1'>
-													<li>Your password must be at least 8 characters</li>
-												</ul>
-											</div>
-										</div>
-									</div>
-								</div>
 							</div>
 							<div>
 								<label htmlFor='email-register' className='block text-sm font-medium text-gray-700'>
@@ -127,7 +51,7 @@ function HomeGuest() {
 								</label>
 								<div className='mt-1'>
 									<input
-										onChange={e => dispatch({ type: 'emailImmediately', value: e.target.value })}
+										onChange={e => setEmail(e.target.value)}
 										id='email-register'
 										name='email'
 										type='email'
@@ -144,7 +68,7 @@ function HomeGuest() {
 								</label>
 								<div className='mt-1'>
 									<input
-										onChange={e => dispatch({ type: 'passwordImmediately', value: e.target.value })}
+										onChange={e => setPassword(e.target.value)}
 										id='password-register'
 										name='password'
 										type='password'
